9afb71a6d3941d140717ba58d3b179a29fe0c830
Martin Barker <martinbarker99@gmail.com>
2025-01-07 14:51:46 -0800

    Updates

---
+++ precache-manifest.d557961301a2f647a830263b8323cff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63c53e5369ae15ce3369f426a63d200b",
+    "url": "/MartinBarker/MartinBarker.github.io/index.html"
+  },
+  {
+    "revision": "438f398db3f0a632b8ef",
+    "url": "/MartinBarker/MartinBarker.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3dd2a499a64d788c0aac",
+    "url": "/MartinBarker/MartinBarker.github.io/static/js/2.f3aeb7a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MartinBarker/MartinBarker.github.io/static/js/2.f3aeb7a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "438f398db3f0a632b8ef",
+    "url": "/MartinBarker/MartinBarker.github.io/static/js/main.8480770a.chunk.js"
+  },
+  {
+    "revision": "a3bdf7ba7939164b152c",
+    "url": "/MartinBarker/MartinBarker.github.io/static/js/runtime-main.54ad79a2.js"
+  }
+]);
